6417b5932a66d5d15bd0110a07f9d91c73a71f8b
Ranbut <Ranbut_Beater@protonmail.com>
2023-04-19 12:28:43 -0300

    Merge branch 'main' of https://github.com/T9-Driven/drivent-api

--- src/services/payments-service/index.ts
+++ src/services/payments-service/index.ts
@@ -1,42 +1,45 @@
-import { Payment } from '@prisma/client';
-import { invalidDataError, notFoundError, unauthorizedError } from '@/errors';
-import { ProcessPaymentWithBody } from '@/protocols';
+import { notFoundError, unauthorizedError } from '@/errors';
+import { CardPaymentParams, PaymentParams } from '@/protocols';
+import enrollmentRepository from '@/repositories/enrollment-repository';
 import paymentsRepository from '@/repositories/payments-repository';
 import ticketsRepository from '@/repositories/tickets-repository';
 
-async function getPayment(ticketId: number, userId: number): Promise<Payment> {
-  if (!ticketId) throw invalidDataError(['Ticket ID not given']);
-
-  const ticket = await ticketsRepository.getUserTicket(userId);
+async function verifyTicketAndEnrollment(ticketId: number, userId: number) {
+  const ticket = await ticketsRepository.findTickeyById(ticketId);
   if (!ticket) throw notFoundError();
 
-  if (userId !== ticket.enrollmentId) throw unauthorizedError();
+  const enrollment = await enrollmentRepository.findById(ticket.enrollmentId);
+  if (!enrollment) throw notFoundError();
 
-  const payment = await paymentsRepository.getUserPayment(ticketId);
+  if (enrollment.userId !== userId) throw unauthorizedError();
+}
+
+async function getPaymentByTicketId(userId: number, ticketId: number) {
+  await verifyTicketAndEnrollment(ticketId, userId);
+
+  const payment = await paymentsRepository.findPaymentByTicketId(ticketId);
   if (!payment) throw notFoundError();
 
   return payment;
 }
 
-async function processPayment(body: ProcessPaymentWithBody, userId: number): Promise<Payment> {
-  if (!body.cardData || !body.ticketId)
-    throw invalidDataError(['Body data is incorrect. Verify if is', 'Incorrect ticketId', 'Incorrect cardData']);
+async function paymentProcess(ticketId: number, userId: number, cardData: CardPaymentParams) {
+  await verifyTicketAndEnrollment(ticketId, userId);
 
-  const ticket = await ticketsRepository.getUserTicket(userId);
-  if (!ticket) throw notFoundError();
+  const ticket = await ticketsRepository.findTickeWithTypeById(ticketId);
 
-  if (userId !== ticket.enrollmentId) throw unauthorizedError();
+  const paymentData: PaymentParams = {
+    ticketId,
+    value: ticket.TicketType.price,
+    cardIssuer: cardData.issuer,
+    cardLastDigits: cardData.number.toString().slice(-4),
+  };
 
-  const paymentProcess = await paymentsRepository.processUserPayment(body, ticket.TicketType.price);
+  const payment = await paymentsRepository.createPayment(ticketId, paymentData);
 
-  await ticketsRepository.createUserTicket(ticket.ticketTypeId, ticket.enrollmentId, 'PAID', ticket.id);
+  await ticketsRepository.ticketProcessPayment(ticketId);
 
-  return paymentProcess;
+  return payment;
 }
 
-const paymentsService = {
-  getPayment,
-  processPayment,
-};
-
-export default paymentsService;
+export default { getPaymentByTicketId, paymentProcess };
